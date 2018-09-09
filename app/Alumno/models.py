60174cda16510fa9936547b06e1dbb6ec4cd324e
Diego <diego.olvera97@gmail.com>
2018-09-08 23:01:12 -0500

    actualizar index y modelos1

--- app/Alumno/models.py
+++ app/Alumno/models.py
@@ -5,19 +5,13 @@ from django.db import models
 
 from django.utils.encoding import smart_str, smart_unicode
 
-from multiselectfield import MultiSelectField
-
 # Create your models here.
 class Alumno(models.Model):
     idAlumno = models.PositiveSmallIntegerField(primary_key = True, null = False, blank = False,)
     nombre = models.CharField(max_length = 50, null = False, blank = False)
     apePat = models.CharField(max_length = 50, null = False, blank = False)
     apeMat = models.CharField(max_length = 50, null = False, blank = False)
-    CHOICE = (
-            ('0', '0'),
-            ('1', '1'),
-            )
-    asesorias = MultiSelectField(choices = CHOICE)
+    asesorias = models.PositiveSmallIntegerField(null = True, blank = True)
     asesoriaDesc = models.CharField(max_length = 255, null = True, blank = True,)
 
     def __str__(self):
